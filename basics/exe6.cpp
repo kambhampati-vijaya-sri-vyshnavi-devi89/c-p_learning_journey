1db2d13674f21da778d5ccb35ae676434b0e3116
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-27 18:20:59 +0530

    Create exe6.cpp

---
+++ basics/exe6.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ 2d arrays
+*/
+int main()
+{
+    int arr[3][5];//3 rows and 5 columns
+    //initialized
+    arr[1][3] = 78 ;
+    //for not initialized place if we return them the garbage value will get
+    cout << arr[1][2];
+    return 0;
+    //o/p: 1980951104
+}
