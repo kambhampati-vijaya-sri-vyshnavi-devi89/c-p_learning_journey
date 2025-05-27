41bdc4e65a67a9c1b26fd6f7f6795e94408ec959
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-27 18:34:33 +0530

    Create exe9.cpp

---
+++ basics/exe9.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ for loops
+*/
+int main()
+{
+    int i;
+    for (i = 5; i > 0; i--)
+    {
+        cout << "sita" << i << endl;
+    }
+    return 0;
+}
