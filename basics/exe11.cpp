9348146196170591be7612726d8a20600e06b5f7
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-27 18:42:29 +0530

    Create exe11.cpp

---
+++ basics/exe11.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ do-while loops
+*/
+int main()
+{
+    int i = 2;
+    do
+    {
+        cout << "sita" << i << endl;
+        i++;
+    } while (i <= 1);
+    return 0;
+}
