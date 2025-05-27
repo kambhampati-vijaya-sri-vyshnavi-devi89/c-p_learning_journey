ebc2f4ede8ed09105d1a0e3d712857d90d04e2aa
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-27 15:51:10 +0530

    Create age_check.cpp

---
+++ basics/age_check.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cin >> age;
+    if(age >= 18){
+        cout << "you are an adult";
+    }
+    else{
+        cout << "you are a minor";
+    }
+    return 0;
+}
