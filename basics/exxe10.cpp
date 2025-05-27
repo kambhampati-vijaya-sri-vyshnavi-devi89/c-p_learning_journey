265ad86e3c45cbf90159d97f7933a6e5e8f21237
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-27 18:37:49 +0530

    Create exxe10.cpp

---
+++ basics/exxe10.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ while loops
+*/
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        cout << "sita" << i << endl;
+        i++;
+    }
+    return 0;
+}
