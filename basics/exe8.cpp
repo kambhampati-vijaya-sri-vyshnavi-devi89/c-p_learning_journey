26ba6c711fe84c9eb8ee6d955ff5250f9e1256fb
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-27 18:32:36 +0530

    Create exe8.cpp

---
+++ basics/exe8.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ for loops
+ output:
+ for loops
+for loops
+for loops
+for loops
+for loops
+for loops
+for loops
+for loops
+for loops
+
+
+*/
+int main()
+{
+    for (int i = 1; i < 10; i++)
+    {
+        cout << "for loops" << endl;
+    }
+    return 0;
+}
