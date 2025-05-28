3d8a3e0a1b8d6d8f9d9bf6ff458741a0763bf802
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-28 11:50:28 +0530

    Create exe7.cpp

---
+++ patterns/exe7.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+*
+***
+ *****
+ *******
+ *********
+ */
+void pattern1(int n)
+{
+    for (int i = 0; i <= n; i++)
+    { // space
+        for (int j = 0; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 1; j <= i * 2 + 1; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 0; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern1(n);
+    }
+
+    return 0;
+}
