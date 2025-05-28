2bbac3e9d119c8dd40fa7208fb8ef774c2ce9dc3
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-28 19:55:55 +0530

    Create exe15.cpp

---
+++ patterns/exe15.cpp
@@ -0,0 +1,31 @@
+void pattern15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern15(n);
+    }
+
+    return 0;
+}
+/*
+A B C D E 
+A B C D 
+A B C 
+A B 
+A
+*/
