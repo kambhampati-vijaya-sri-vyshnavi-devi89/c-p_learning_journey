c3c9408e17145c6e54c154168732030641bd76ad
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-28 11:58:13 +0530

    Update exe7.cpp

--- patterns/exe7.cpp
+++ patterns/exe7.cpp
@@ -9,7 +9,7 @@ using namespace std;
  */
 void pattern1(int n)
 {
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     { // space
         for (int j = 0; j <= n - i - 1; j++)
         {
