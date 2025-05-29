ffe19fb9ac293c965b29219ff370dfa6f4d0bce4
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-05-29 06:09:26 +0530

    Create exe18.cpp

---
+++ patterns/exe18.cpp
@@ -0,0 +1,32 @@
+void pattern17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern17(n);
+    }
+
+    return 0;
+}
+/*
+E 
+D E 
+C D E 
+B C D E 
+A B C D E 
+
+*/
