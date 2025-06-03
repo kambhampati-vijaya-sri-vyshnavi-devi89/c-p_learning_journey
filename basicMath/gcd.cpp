a141faf93ee0522d914ce68723bee78c00d6563c
vijaya sri vyshanvi devi kambhamapati <kvsvdevi@gmail.com>
2025-06-03 06:48:52 +0530

    Create gcd.cpp

---
+++ basicMath/gcd.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+            a = a % b;
+        else
+            b = b % a;
+    }
+    if (a == 0)
+    {
+        cout << "gcd" << b << endl;
+    }
+    else
+    {
+        cout << "gcd" << a << endl;
+    }
+}
+
+int main()
+
+{
+    int a, b;
+    cin >> a >> b;
+    gcd(a, b);
+    return 0;
+}
